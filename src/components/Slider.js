3ab00ba2d9de2440f26ad0e62a00b8105c74c500
Glitch (photon-controller) <none>
2018-04-17 16:10:54 +0000

    Changed to veritical slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-import Slider, { Range } from 'rc-slider';
+import Slider  from 'rc-slider';
 // We can just import Slider or Range to reduce bundle size
 // import Slider from 'rc-slider/lib/Slider';
 // import Range from 'rc-slider/lib/Range';
 import 'rc-slider/assets/index.css';
- 
+const style = {float: 'left', width: 200, height: 400, marginBottom: 160, marginLeft: 50};
+const parentStyle = {overflow: 'hidden'};
 class PhotonSlider extends React.Component {
   render () {
-  return <div>
-    <Slider />
-    <Range />
+  return <div style={style}>
+    <Slider vertical dots step={20} defaultValue={100}/>
+    
   </div>;
   }
 }
