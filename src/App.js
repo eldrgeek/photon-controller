46019cf6ed9fd374659b82680b80d3db0a2bb29b
Glitch (photon-controller) <none>
2018-04-18 14:08:39 +0000

    Refactored

--- src/App.js
+++ src/App.js
@@ -7,42 +7,59 @@ import Button from "./components/Button"
 import PhotonSlider from "./components/Slider"
 // class Button extends React.Component {
 
-//   render() {
-//     const { 
-//       variant,
-//       content,
-//       ...others
-//     } = this.props;
-    
-//     return (
-//       <button className={variant} {...others}>
-//         {content}
-//       </button>
-//     )
-//   }
-// }
 
-
-const sendCommand = () => {
-    console.log("pressed")
-     fetch("https://api.particle.io/v1/devices/2d0028000b47353235303037/dccCommand", {
-          body: "arg=headlight,on&access_token=7272794e183736c5a14dce7ebd8ace2fb6fe5e56",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          method: "POST"
-        }).then((myJson) => {
-    console.log("Success")
-    console.log(myJson);
-  }).catch(error => console.error(error))
-
-}
+let colors = {
+    sending: "yellow",
+    ready: "green",
+    error: "red"
+  };
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onPressed: "ready",
+      offPresssed: "ready"
+    };
+    this.sendDcc("on");
+    setTimeout ( (() => this.sendDcc("off")), 1000)
+    
+  }
+  getColor(command){
+    return colors[command] || "red"
+  }
+  sendDcc(command) {
+    return this.sendWithArgs("dcc", command)
+  }
+  sendWithArgs(type, command) {
+    console.log("pressed", command);
+    fetch(`https://api.particle.io/v1/devices/2d0028000b47353235303037/${type}Command`, {
+            body: `arg=headlight,${command}&access_token=7272794e183736c5a14dce7ebd8ace2fb6fe5e56`,
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            method: "POST"
+          }).then((myJson) => {
+      console.log("Success")
+      console.log(myJson);
+      window.myJson = myJson
+      if(!myJson.ok) {throw new Error("not OK")}
+      this.setState({[command + "Pressed"]:"ready"})
+    }).catch(error => {console.error(error)
+    this.setState({[command + "Pressed"]:"error"})
+      })
+  }
   render() {
     
     return <div>
-      <Button content="Send it!" onClick={sendCommand} variant="green" />
-        <Button content="Reverse It" onClick={sendCommand} variant="red" />
+      <Button content="New Way" onClick={() => {
+      this.setState({onPressed:"sending"});this.sendDcc("on")}
+      } 
+      variant={this.getColor(this.state.onPressed)} />
+<Button content="New Off" onClick={() => {
+      this.setState({onPressed:"sending"});this.sendDcc("off")}
+      } 
+      variant={this.getColor(this.state.onPressed)} />
+
           <PhotonSlider />
         </div>
   }    
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,11 +6,11 @@ import Slider  from 'rc-slider';
 // import Range from 'rc-slider/lib/Range';
 import 'rc-slider/assets/index.css';
 const style = {float: 'left', width: 200, height: 400, marginBottom: 160, marginLeft: 50};
-const parentStyle = {overflow: 'hidden'};
+//const parentStyle = {overflow: 'hidden'};
 class PhotonSlider extends React.Component {
   render () {
   return <div style={style}>
-    <Slider vertical dots step={20} defaultValue={100}/>
+    <Slider vertical dots step={1} defaultValue={126}/>
     
   </div>;
   }
